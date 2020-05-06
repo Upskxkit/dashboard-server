344fa6a67129b5761b7ba2e45d895ab0596448cb
upskxkit <Rekoj1893>
2020-05-06 20:57:53 +0300

    test port

--- server.ts
+++ server.ts
@@ -2,9 +2,11 @@ import {appServer} from "./lib/server";
 
 const port = process.env.PORT || 8080;
 
+console.log(port);
+
 const server = new appServer({
-    "transport": "ws",
-    "port": +port,
-    "host": "localhost"
+  "transport": "ws",
+  "port": +port,
+  "host": "localhost"
 }, () => {
 });
